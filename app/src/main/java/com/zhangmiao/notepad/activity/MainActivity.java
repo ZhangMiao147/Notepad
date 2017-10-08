b21991f657115a819f380dad0fb5609743469b48
zhangmiao <zhangmiao.dream@qq.com>
2017-10-08 14:28:55 +0800

    1.”主界面“、“我的笔记”、“我的心情”修改为fragment实现； 2.修改一些简单的图标。

--- app/src/main/java/com/zhangmiao/notepad/activity/MainActivity.java
+++ app/src/main/java/com/zhangmiao/notepad/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.zhangmiao.notepad.activity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -10,14 +12,15 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 import com.zhangmiao.notepad.R;
 import com.zhangmiao.notepad.adapter.LeftListViewAdapter;
+import com.zhangmiao.notepad.fragment.MainFragment;
+import com.zhangmiao.notepad.fragment.MyMoodFragment;
+import com.zhangmiao.notepad.fragment.MyNoteFragment;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -56,14 +59,34 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.main_drawer_layout)
     DrawerLayout mDrawerLayout;
 
+    FragmentManager mManager;
+
+    MainFragment mMainFragment;
+
+    MyNoteFragment mNoteFragment;
+
+    MyMoodFragment mMoodFragment;
+
+    Menu mMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setDefaultFragment();
         setToolbar();
         initListView();
+    }
+
+    private void setDefaultFragment() {
+        if (mManager == null) {
+            mManager = getFragmentManager();
+        }
+        FragmentTransaction transaction = mManager.beginTransaction();
+        mMainFragment = new MainFragment();
+        transaction.replace(R.id.main_fragment, mMainFragment);
+        transaction.commit();
     }
 
     private void initListView() {
@@ -79,11 +102,35 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "mLeftRecordListView position = " + position);
         if (position == 0) {
             //我的笔记
-            startActivity(new Intent(MainActivity.this, MyNoteActivity.class));
+            //startActivity(new Intent(MainActivity.this, MyNoteActivity.class));
+            if (mManager == null) {
+                mManager = getFragmentManager();
+            }
+            FragmentTransaction transaction = mManager.beginTransaction();
+            mNoteFragment = new MyNoteFragment();
+            transaction.replace(R.id.main_fragment, mNoteFragment);
+            transaction.commit();
+            if (mMenu == null) {
+                mMenu = mToolbar.getMenu();
+            }
+            mMenu.findItem(R.id.menu_main_search).setVisible(true);
+            mToolbar.setTitle("我的笔记");
         }
         if (position == 1) {
             //我的心情
-            startActivity(new Intent(MainActivity.this, MyMoodActivity.class));
+            //startActivity(new Intent(MainActivity.this, MyMoodActivity.class));
+            if (mManager == null) {
+                mManager = getFragmentManager();
+            }
+            FragmentTransaction transaction = mManager.beginTransaction();
+            mMoodFragment = new MyMoodFragment();
+            transaction.replace(R.id.main_fragment, mMoodFragment);
+            transaction.commit();
+            if (mMenu == null) {
+                mMenu = mToolbar.getMenu();
+            }
+            mMenu.findItem(R.id.menu_main_search).setVisible(true);
+            mToolbar.setTitle("我的心情");
         }
 
         if (position == 2) {
@@ -95,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
     @OnItemClick(R.id.main_left_set)
     public void LeftSetItemClick(AdapterView<?> parent, View view, int position, long id) {
         Log.d(TAG, "LeftSetItemClick position = " + position);
-
+        //意见反馈与设置
         mDrawerLayout.closeDrawer(Gravity.LEFT);
     }
 
@@ -143,7 +190,15 @@ public class MainActivity extends AppCompatActivity {
 
     private void setToolbar() {
         mToolbar.setTitle("");
+        mToolbar.setNavigationIcon(R.drawable.menu);
         setSupportActionBar(mToolbar);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
+
     }
 
     @OnClick(R.id.main_add)
@@ -165,14 +220,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(this, WriteMoodActivity.class));
     }
 
-    @OnClick(R.id.main_toolbar_menu)
-    public void showLeftMenu() {
-        mDrawerLayout.openDrawer(Gravity.LEFT);
-    }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        mMenu = menu;
+        mMenu.findItem(R.id.menu_main_search).setVisible(false);
         return true;
     }
 
---
+++ app/src/main/java/com/zhangmiao/notepad/fragment/MyMoodFragment.java
@@ -0,0 +1,23 @@
+package com.zhangmiao.notepad.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zhangmiao.notepad.R;
+
+/**
+ * Author: zhangmiao
+ * Date: 2017/10/8
+ */
+public class MyMoodFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_mood, container, false);
+    }
+}
